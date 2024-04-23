664b960fb0f5a32d9cbd8229a4ad183a612cce75
지준호 <jjho311@naver.com>
2024-04-23 17:28:05 +0900

    Feat: 감정, 감정 레벨을 기반으로 19개 클래스 값을 가짇록 코드 수정

--- AI/emotion/code/pretreatment/conversation_to_output.py
+++ AI/emotion/code/pretreatment/conversation_to_output.py
@@ -12,12 +12,38 @@ output_path = "data/conversation/output/"
 train_file_name = "train_conversation.xlsx"
 validation_file_name = "validation_conversation.xlsx"
 
+# 감정과 그 정도에 따라 숫자 부여
+def getNum(emotion, level):
+    result=-1
+
+    if(emotion=='두려움' and level=='약함'):        result=0
+    elif(emotion=='두려움' and level=='보통'):      result=1
+    elif(emotion=='두려움' and level=='강함'):      result=2
+    elif(emotion=='슬픔' and level=='약함'):        result=3
+    elif(emotion=='슬픔' and level=='보통'):        result=4
+    elif(emotion=='슬픔' and level=='강함'):        result=5
+    elif(emotion=='화남' and level=='약함'):        result=6
+    elif(emotion=='화남' and level=='보통'):        result=7
+    elif(emotion=='화남' and level=='강함'):        result=8
+    elif(emotion=='없음'):                          result=9
+    elif(emotion=='기쁨' and level=='약함'):        result=10
+    elif(emotion=='기쁨' and level=='보통'):        result=11
+    elif(emotion=='기쁨' and level=='강함'):        result=12
+    elif(emotion=='놀라움' and level=='약함'):      result=13
+    elif(emotion=='놀라움' and level=='보통'):      result=14
+    elif(emotion=='놀라움' and level=='강함'):      result=15
+    elif(emotion=='사랑스러움' and level=='약함'):  result=16
+    elif(emotion=='사랑스러움' and level=='보통'):  result=17
+    elif(emotion=='사랑스러움' and level=='강함'):  result=18
+
+    return result
+
 # input 파일들에서 필요한 데이터를 추출하여 하나의 파일로 만드는 메서드
 def setWB(isTrain):
     # 필요한 변수 선언
     input_path = input_train_path if isTrain else input_validation_path
     output_file_path = output_path + (train_file_name if isTrain else validation_file_name)
-    column = ["감정", "감정_레벨", "감정_카테고리"]
+    column = ["Text", "Class"]
 
     # 엑셀 파일 관련 설정
     wb = openpyxl.Workbook()
@@ -25,23 +51,22 @@ def setWB(isTrain):
     ws.title = 'Conversation'
     ws.append(column)
 
-    # 시트 title 지정
-
     # input_path 내 정보 순회
     for (root, directories, files) in os.walk(input_path):
         # input_path 내 모든 퍄일 순회
         for file in files:
-            # 파일을 염
+            # 파일을 열어 데이터 추출
             try:
                 with open(input_path + file, 'r', encoding='UTF-8') as input_file:
                     data = json.load(input_file)
                     conversations = data["Conversation"]
-                    
+
                     for conversation in conversations:
-                        row = [ \
-                            re.sub(r"[^\uAC00-\uD7A30\s]", "", conversation["Text"]), conversation["SpeakerEmotionTarget"], \
-                            conversation["SpeakerEmotionLevel"], conversation["SpeakerEmotionCategory"] \
-                        ]
+                        class_num = getNum(conversation['SpeakerEmotionTarget'], conversation['SpeakerEmotionLevel'])
+                        if(class_num==-1):
+                            print(conversation)
+                            continue
+                        row = [re.sub(r"[^\uAC00-\uD7A30\s]", "", conversation["Text"]), class_num]
                         ws.append(row)
             except:
                 print(file, row)
