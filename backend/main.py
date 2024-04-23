82749f6d88c07ff9a7d30310a2f1d8cffd566d1a
gw07167 <gw07167@naver.com>
2024-04-23 17:01:51 +0900

    feat: selenium 환경 세팅
    Change-Id: I33bbc516d3793c4eb59d9fcc81015e50257b90d2

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 from models.exception.CustomException import CustomException
 
@@ -19,3 +21,12 @@ async def custom_exception_handler(request: Request, exc: CustomException):
 async def root():
     raise CustomException(status_code=404, message="exception test ok")
     return {"message": "Hello World"}
+
+@app.get("/cafe-crawl")
+def cafe_crawl():
+    driver = webdriver.Chrome()
+    url = 'https://cafe.naver.com/cosmania/14985050?art=ZXh0ZXJuYWwtc2VydmljZS1uYXZlci1zZWFyY2gtY2FmZS1wcg.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYWZlVHlwZSI6IkNBRkVfVVJMIiwiY2FmZVVybCI6ImNvc21hbmlhIiwiYXJ0aWNsZUlkIjoxNDk4NTA1MCwiaXNzdWVkQXQiOjE3MTM4NTU3NzQ0ODN9.qFUhsgkWPb4COj8SgJBTOGtIgFc4nq15B-1QEPiBf4E'
+    driver.get(url)
+
+    driver.switch_to.frame("cafe_main")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
