1ee63801e3eb76f92252e16de9d976ea90cd1201
Haeun Kim <gkdms1120s@gmail.com>
2024-04-25 16:03:10 +0900

    Feat: blog 단순 coloring 코드 작성
    Change-Id: Ie2c846523d7c4dc2d42503141cfd877a817fbf9b

---
+++ Extension/ADvice/public/searchBlogDetail.js
@@ -0,0 +1,9 @@
+var iframe = document
+  .getElementById("mainFrame")
+  .contentWindow.document.getElementById(
+    "SE-EEAF51C0-18A7-43DD-9E5D-3D9B1DD0ED5E"
+  );
+
+iframe.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // 초록색
+// iframe.style.backgroundColor = "rgba(241, 43, 67, 0.3)";
+console.log(iframe);
--- Extension/ADvice/public/searchScript.js
+++ Extension/ADvice/public/searchScript.js
@@ -1,4 +1,3 @@
-
 const userInfoElements = document.getElementsByClassName("bx");
 let level = [2, 2, 3, 4, 5];
 const maxLevel = 5;
@@ -27,18 +26,16 @@ window.onclick = function (event) {
   }
 };
 
-
-
-function setUI(node){
-  console.log("setUI 실행")
-  const userInfoElements = node.querySelectorAll(".api_save_group")
+function setUI(node) {
+  console.log("setUI 실행");
+  const userInfoElements = node.querySelectorAll(".api_save_group");
   // const userInfoElements = document.getElementsByClassName("api_save_group");
 
   Array.from(userInfoElements).forEach((element, index) => {
     console.log(element.innerHTML);
     const levelValue = level[index];
     const percentage = (levelValue / maxLevel) * 100; // 최대 단계에 대한 현재 단계의 백분율
-  
+
     // 진행 상태 표시
     const progressBarHTML = `
       <div style="float : right; display : flex; padding : 1% 2%;; border-radius : 15px 15px; border: 1px solid lightgray; box-shadow: 1px 1px 2px lightgray; width : 20%;">
@@ -57,44 +54,42 @@ function setUI(node){
         </div>
       <div>
     `;
-  
+
     element.insertAdjacentHTML("afterend", progressBarHTML);
     element.style.display = "flex";
     const userBox = element.parentNode.querySelector(".user_box_inner");
   });
-  
-  // -------- 유용도 박스
 
+  // -------- 유용도 박스
 
   const links = node.querySelectorAll(".title_area a");
   links.forEach((link) => {
     link.addEventListener("mouseover", function () {
       console.log(link);
       modalText.textContent = `게시글 요약${link}`;
-  
+
       const linkRect = link.getBoundingClientRect();
       modal.style.left = `${linkRect.left + window.scrollX}px`;
       modal.style.top = `${linkRect.bottom + window.scrollY}px`;
       modal.style.display = "block";
-      modal.style.position = 'absolute';
+      modal.style.position = "absolute";
     });
-  
-    link.addEventListener("mouseout", function() {
-      modal.style.display = "none";
-    })
-  });
 
+    link.addEventListener("mouseout", function () {
+      modal.style.display = "none";
+    });
+  });
 }
 
-// ------------ 처음 검색 결과에 ui 씌우기 
+// ------------ 처음 검색 결과에 ui 씌우기
 
 // 콜백 함수 정의
-const callback = function(mutationsList, observer) {
+const callback = function (mutationsList, observer) {
   for (const mutation of mutationsList) {
-    if (mutation.type === 'childList') {
+    if (mutation.type === "childList") {
       mutation.addedNodes.forEach((node) => {
-        if (node.tagName === 'LI' && node.classList.contains('bx')) {
-          console.log('새로운 <li> 태그가 추가됨:', node);
+        if (node.tagName === "LI" && node.classList.contains("bx")) {
+          console.log("새로운 <li> 태그가 추가됨:", node);
           setUI(node);
         }
       });
@@ -107,10 +102,10 @@ const observer = new MutationObserver(callback);
 
 // 관찰 설정: 자식 요소의 변화를 관찰
 const config = { childList: true, subtree: true };
-const targetNode = document.querySelector('.lst_view');
+const targetNode = document.querySelector(".lst_view");
 
 // 변화 관찰 시작
 observer.observe(targetNode, config);
 
 // 페이지가 언로드될 때 옵저버를 해제
-window.addEventListener('unload', () => observer.disconnect());
+window.addEventListener("unload", () => observer.disconnect());
