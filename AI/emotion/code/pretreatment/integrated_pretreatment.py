66f1ad23e5f4544c3835e43435ea973a48b1c6e0
지준호 <jjho311@naver.com>
2024-04-26 12:31:33 +0900

    Refactor : 감정이 태깅괸 자유대화 함수 제거

--- AI/emotion/code/pretreatment/integrated_pretreatment.py
+++ AI/emotion/code/pretreatment/integrated_pretreatment.py
@@ -10,50 +10,6 @@ output_path = "./AI/emotion/data/output/pretreated_data.csv"
 # 컬럼 정보
 columns = ['Text', 'Emotion']
 
-
-# 감정이 태깅된 자유대화 데이터 전처리 함수
-def freeConv2Data():
-    # 화자 감정에 따라 부정/중립/긍정 반환
-    def getEmotion(emotion_target, emotion_level):
-        if emotion_target == "없음" or emotion_level == "약함":
-            return "중립"
-        elif emotion_target in ["두려움", "슬픔", "화남"]:
-            return "부정"
-        elif emotion_target in ["기쁨", "놀라움", "사랑스러움"]:
-            return "긍정"
-        else:
-            return -1
-
-    # input_path 내 파일 순회
-    folder_name = "감정이_태깅된_자유대화/"
-    data_list = []
-    for (root, directories, files) in os.walk(input_path + folder_name):
-        print(len(files))
-        # input_path 내 모든 퍄일 순회
-        for file in files:
-            # 파일을 열어 데이터 추출
-            with open(input_path + folder_name + file, 'r', encoding='UTF-8') as input_file:
-                data = json.load(input_file)
-                conversations = data["Conversation"]
-
-                for conversation in conversations:
-                    text = conversation["Text"]
-                    if type(text)!=str: 
-                        print(file, text)
-                        exit(-1)
-                    emotion = getEmotion(conversation['SpeakerEmotionTarget'], conversation['SpeakerEmotionLevel'])
-                    if(emotion==-1):    
-                        print(file, text)
-                        exit(-1)
-                    row = [text, emotion]
-
-                    try:
-                        data_list.append(row)
-                    except:
-                        continue
-
-    pd.DataFrame(data_list, columns=columns).to_csv(output_path, index=False, mode='a', encoding='utf-8-sig')
-
 # 한국어 단발성 대화 데이터셋
 def oneShotConv2Data():
     file_name = "한국어_단발성_대화_데이터셋.xlsx"
@@ -104,5 +60,3 @@ def attrBased2Data():
 # 전처리 시작
 oneShotConv2Data()
 attrBased2Data()
-
-# 파일 저장 및 종료
