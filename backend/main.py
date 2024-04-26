ad8d164a366ba28f72e0415bee1d7696cc7431d9
gw07167 <gw07167@naver.com>
2024-04-26 10:21:27 +0900

    feat: selenium 속도향상
    Change-Id: Idd7d70b96a1efdce69056cdff9dd8f9e70cff821

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-import datetime
 from bs4 import BeautifulSoup
 
 from models.exception.CustomException import CustomException
@@ -30,20 +31,13 @@ async def root():
 def cafe_crawl(url: str):
     start = time.time()
     options = webdriver.ChromeOptions()
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("headless")
-    options.add_argument("--disable-gpu")
-    caps = DesiredCapabilities.CHROME
-    caps["pageLoadStrategy"] = "none"
-    # options.add_argument(
-    #     'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-    # )
     driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome()
+
     driver.get(url)
     time.sleep(1)
 
     driver.switch_to.frame("cafe_main")
+    driver.quit()
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
     #print(soup.select('.se-main-container'))
@@ -84,16 +78,44 @@ def cafe_crawl(url: str):
 def cafe_crawl2(url: str):
     start = time.time()
     options = webdriver.ChromeOptions()
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--blink-settings=imagesEnabled=false");
-    # caps = DesiredCapabilities.CHROME
-    # caps["pageLoadStrategy"] = "none"
-    driver = webdriver.Chrome(options=options)
+    options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--log-level=3')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--incognito')
+    options.add_argument('--disable-images')
+
+    # 속도 향상을 위한 옵션 해제
+    prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
+                                                        'geolocation': 2, 'notifications': 2,
+                                                        'auto_select_certificate': 2, 'fullscreen': 2, 'mouselock': 2,
+                                                        'mixed_script': 2, 'media_stream': 2, 'media_stream_mic': 2,
+                                                        'media_stream_camera': 2, 'protocol_handlers': 2,
+                                                        'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+                                                        'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                        'metro_switch_to_desktop': 2, 'protected_media_identifier': 2,
+                                                        'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2}}
+    options.add_experimental_option('prefs', prefs)
+
+    options.add_argument('--blink-settings=imagesEnabled=false')
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+    options.add_argument(f'user-agent={user_agent}')
+
+    caps = DesiredCapabilities.CHROME
+    caps["pageLoadStrategy"] = "none"
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # driver = webdriver.Chrome()
     driver.get(url)
+
     time.sleep(1)
+    # driver.implicitly_wait(3)
 
     driver.switch_to.frame("cafe_main")
     soup = BeautifulSoup(driver.page_source, "html.parser")
