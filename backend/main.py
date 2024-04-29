2317c576a8761956de44ed86dc1ecfea9c15c325
지준호 <jjho311@naver.com>
2024-04-29 22:41:07 +0900

    Fix: 커스텀 예외처리 클래스 폴더명 변경에 의한 오류 수정

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
-
-from models.exception.custom_exception import CustomException
+from domain.exception.custom_exception import CustomException
 
 app = FastAPI()
 
