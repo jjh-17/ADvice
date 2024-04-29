0384f48210df48188b6e93a5c54753bb01b3ceb1
gw07167 <gw07167@naver.com>
2024-04-29 12:32:48 +0900

    feat: selenium 속도 향상 제거
    Change-Id: I4b1a81625e2e7bb1e06426262d6f7491f534cf3b

--- backend/services/naver_cafe_scrap.py
+++ backend/services/naver_cafe_scrap.py
@@ -15,44 +15,45 @@ class NaverCafeScrapper:
         self.driver = None
 
     def create_webdriver(self):
-        options = webdriver.ChromeOptions()
-        options.headless = True
-        options.add_argument('--headless')
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--log-level=3')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--disable-infobars')
-        options.add_argument('--disable-extensions')
-        options.add_argument('--incognito')
-        options.add_argument('--disable-images')
+        # options = webdriver.ChromeOptions()
+        # options.headless = True
+        # options.add_argument('--headless')
+        # options.add_argument('--ignore-ssl-errors=yes')
+        # options.add_argument('--ignore-certificate-errors')
+        # options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--no-sandbox')
+        # options.add_argument('--log-level=3')
+        # options.add_argument('--disable-gpu')
+        # options.add_argument('--disable-infobars')
+        # options.add_argument('--disable-extensions')
+        # options.add_argument('--incognito')
+        # options.add_argument('--disable-images')
+        #
+        # # 속도 향상을 위한 옵션 해제
+        # prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
+        #                                                     'geolocation': 2, 'notifications': 2,
+        #                                                     'auto_select_certificate': 2, 'fullscreen': 2,
+        #                                                     'mouselock': 2,
+        #                                                     'mixed_script': 2, 'media_stream': 2, 'media_stream_mic': 2,
+        #                                                     'media_stream_camera': 2, 'protocol_handlers': 2,
+        #                                                     'ppapi_broker': 2, 'automatic_downloads': 2,
+        #                                                     'midi_sysex': 2,
+        #                                                     'push_messaging': 2, 'ssl_cert_decisions': 2,
+        #                                                     'metro_switch_to_desktop': 2,
+        #                                                     'protected_media_identifier': 2,
+        #                                                     'app_banner': 2, 'site_engagement': 2,
+        #                                                     'durable_storage': 2}}
+        # options.add_experimental_option('prefs', prefs)
+        #
+        # options.add_argument('--blink-settings=imagesEnabled=false')
+        # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+        # options.add_argument(f'user-agent={user_agent}')
+        #
+        # caps = DesiredCapabilities.CHROME
+        # caps["pageLoadStrategy"] = "none"
 
-        # 속도 향상을 위한 옵션 해제
-        prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
-                                                            'geolocation': 2, 'notifications': 2,
-                                                            'auto_select_certificate': 2, 'fullscreen': 2,
-                                                            'mouselock': 2,
-                                                            'mixed_script': 2, 'media_stream': 2, 'media_stream_mic': 2,
-                                                            'media_stream_camera': 2, 'protocol_handlers': 2,
-                                                            'ppapi_broker': 2, 'automatic_downloads': 2,
-                                                            'midi_sysex': 2,
-                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
-                                                            'metro_switch_to_desktop': 2,
-                                                            'protected_media_identifier': 2,
-                                                            'app_banner': 2, 'site_engagement': 2,
-                                                            'durable_storage': 2}}
-        options.add_experimental_option('prefs', prefs)
-
-        options.add_argument('--blink-settings=imagesEnabled=false')
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-        options.add_argument(f'user-agent={user_agent}')
-
-        caps = DesiredCapabilities.CHROME
-        caps["pageLoadStrategy"] = "none"
-
-        driver = webdriver.Chrome(options=options)
+        # driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome()
         return driver
 
     def initialize_driver(self):
