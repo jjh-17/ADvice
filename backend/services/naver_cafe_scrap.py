0878cdcb6766cddf6564997a45990d15967680a7
gw07167 <gw07167@naver.com>
2024-04-29 13:26:21 +0900

    feat: selenium chromeDriver service install
    Change-Id: I7b2f68af47faf9e4a7663379098d0287940fe87f

--- backend/services/naver_cafe_scrap.py
+++ backend/services/naver_cafe_scrap.py
@@ -52,8 +52,12 @@ class NaverCafeScrapper:
         # caps = DesiredCapabilities.CHROME
         # caps["pageLoadStrategy"] = "none"
 
-        # driver = webdriver.Chrome(options=options)
-        driver = webdriver.Chrome()
+        # ChromeDriver 경로 지정 및 옵션 설정
+        driver_path = "./chromedriver-win64/chromedriver.exe"
+        service = Service(executable_path=driver_path)
+
+        # 웹드라이버 초기화
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         return driver
 
     def initialize_driver(self):
