4560bc64f2688e7e29e64562a2bab766c601feb3
이인석 <inkcloud365@gmail.com>
2024-04-30 10:24:48 +0900

    Feat: 문장 재배치 로직 추가
    Change-Id: I6e2931bf1262e56c4a3ed1d039d3d94b612a2e26

--- backend/internals/detail_evaluation.py
+++ backend/internals/detail_evaluation.py
@@ -11,12 +11,7 @@ class AdEvaluation:
         data = self.__split_string(paragraph)
         ad_result = self.__detector.predict(data)
 
-        result = []
-        for i in range(len(data)):
-            if ad_result[i] == 1:
-                result = result.append(data[i])
-
-        return "".join(result)
+        return data, ad_result
 
     def __split_string(self, paragraph: str):
         sentences = split_sentences(paragraph)
