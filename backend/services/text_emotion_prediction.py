a28460e537466e04b0b7553f0c5f2166075a9d39
지준호 <jjho311@naver.com>
2024-05-02 14:11:02 +0900

    Feat: 모델 자체가 아닌, weight를 load하는 형식으로 변경

--- backend/services/text_emotion_prediction.py
+++ backend/services/text_emotion_prediction.py
@@ -52,8 +52,6 @@ class TextEmotionPrediction:
     # 초기화
     def __init__(self):
         self.device = torch.device('cpu')
-        # self.model = (torch.load("./model/KoBERT_emo/emotion_classification_model.pt", map_location=self.device)
-        #               .to(self.device))
         bert_model = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False).to(self.device)
         self.model = BERTClassifier(bert_model, dr_rate=0.5).to(self.device)
         self.model.load_state_dict(torch.load('./model/KoBERT_emo/emotion_classification_weight.pt', map_location=self.device))
@@ -72,20 +70,11 @@ class TextEmotionPrediction:
                 cnt_neu += 1
             else:
                 cnt_pos += 1
-
+            print(text + " / " + str(result) + "\n")
         return [cnt_neg, cnt_neu, cnt_pos]
 
     #  부정 : -1, 중립 : 0, 긍정 : 1
     def sentence_predict(self, sentence):
-        # 실수를 치역으로 한 가중치 값을 softmax함수를 사용하여 텍스트를 확률값으로 변환
-        def new_softmax(logits):
-            c = np.max(logits)  # 최댓값
-            exp_logits = np.exp(logits - c)  # 각각의 원소에 최댓값을 뺀 값에 exp를 취한다. (이를 통해 overflow 방지)
-            sum_exp_logits = np.sum(exp_logits)
-            y = (exp_logits / sum_exp_logits) * 100
-
-            return np.round(y, 3)
-
         data = [sentence, '0']
         dataset_another = [data]
         input_dataset = BERTDataset(dataset_another, 0, 1, self.tok, self.vocab, 64, True, False)  # 토큰화한 문장
@@ -97,10 +86,7 @@ class TextEmotionPrediction:
             token_ids = token_ids.long().to(self.device)
             segment_ids = segment_ids.long().to(self.device)
             valid_length = valid_length
-            label = label.long().to(self.device)
-
             with torch.no_grad():
                 output = self.model(token_ids, valid_length, segment_ids)
                 logits = output.detach().cpu().numpy()
-                # logits = np.round(new_softmax(logits), 3).tolist()
         return np.argmax(logits) - 1
