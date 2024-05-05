a6899fad7e70743b1b228bc35a07a86114a6c113
김하은 <gkdms1120s@gmail.com>
2024-05-05 16:51:47 +0900

    Feat: Option별 Response 분리 적용, hover시 Option 이름 띄워주기

--- Extension/ADvice/public/searchBlogDetail.js
+++ Extension/ADvice/public/searchBlogDetail.js
@@ -1,20 +1,187 @@
+// 테스트 블로그 : https://blog.naver.com/gospel416/223425484859
+
 var iframe = document.getElementById("mainFrame");
+var optionName = [
+  "사진(image)/영상(video)/링크/지도(placeMap)/ 등 다양성",
+  "구매 링크나 특정 사이트로의 유도 링크가 포함되어 있는 경우",
+  "내돈내산 인증 포함",
+  "특정 키워드 포함",
+  "광고 문구",
+  "장점/단점 비율",
+  "인위적인 사진 포함",
+  "객관적인 정보(영업시간, 장소위치, 가격 포함)",
+  "상세한 설명",
+  "이모티콘 개수",
+];
+
+var selectedOption = [1, 2];
+var tmpData = {
+  adDetection: [
+    {
+      option: 1,
+      goodList: [
+        {
+          id: "SE-600b5534-5478-40eb-93f6-66704272a14d",
+          data: ["겹벚꽃 보러,"],
+        },
+        {
+          id: "SE-e7e63fcf-f41c-439a-b75d-a67fdba06c27",
+          data: [
+            "3월 말부터 4월 초에 제주여행을 다녀왔다.",
+            "제주 여행 내내 비가 와서 제대로 벚꽃을 보지 못했는데, 서울에 오니 이미 벚꽃 엔딩이다.",
+          ],
+        },
+      ],
+      badList: [
+        {
+          id: "SE-0dd3e82e-a5c4-4d0f-b623-eb87cc6fcfee",
+          data: [
+            "정말 아쉬웠다.",
+            "벚꽃을 이렇게 보내다니.",
+            "그래서 겹벚꽃 개화를 기다렸다.",
+            '그러다 막냇동생이 "언니 어린이대공원에 겹벚꽃 만개했대"',
+            "해서 알아보니 벌써 활짝 폈다.",
+          ],
+        },
+        {
+          id: "SE-6ff4b4f4-68cb-49cf-b59d-e265e69aaa18",
+          data: [
+            "급하게 날짜를 잡고 함께 다녀왔다.",
+            "겹벚꽃 소식을 알려준 막내는 약속이 있어 못 가고 첫째 동생이랑 조카와 함께 다녀왔다.",
+          ],
+        },
+        {
+          id: "SE-8d9613c4-de90-4f58-ac2f-e944a918f834",
+          data: [
+            "요즘 꽃구경 다닐 때마다 인간 네비가 됐다.",
+            "우리는 조카가 있어서 주차를 하고 나와 분수대 쪽으로 나왔다.",
+            "분수를 앞에 두고 볼 때 왼편으로 걸어가면 바로 핑크빛 겹벚꽃을 만날 수 있다.",
+          ],
+        },
+      ],
+    },
+    {
+      option: 2,
+      goodList: [
+        {
+          id: "SE-8d9613c4-de90-4f58-ac2f-e944a918f834",
+          data: [
+            "요즘 꽃구경 다닐 때마다 인간 네비가 됐다.",
+            "우리는 조카가 있어서 주차를 하고 나와 분수대 쪽으로 나왔다.",
+            "분수를 앞에 두고 볼 때 왼편으로 걸어가면 바로 핑크빛 겹벚꽃을 만날 수 있다.",
+          ],
+        },
+        {
+          id: "SE-e569d924-0bb3-4da4-95fa-62e24b1c25f5",
+          data: [
+            "조금 놀랐던 건 작년 빼고 매해 방문했는데, 더 풍성했던 걸로 기억하는데 원래 찍던 나무는 꽃이 거의 없고 다른 장소에 한 그루 정도 낮게 있었다.",
+          ],
+        },
+      ],
+      badList: [
+        {
+          id: "SE-b785602a-1c29-406b-9794-2d375bda5854",
+          data: [
+            "큰 겹벚꽃 나무는 있는데 사진 찍을 만큼 낮으면서 풍성한 나무가 많이 사라진 느낌이었다.",
+            "내가 늦게 방문해서 겹벚꽃이 진 걸까 아니면 그냥 피지 못한 걸까 궁금했다.",
+          ],
+        },
+        {
+          id: "SE-d1b962d4-7b7b-4b30-b1d4-9b4eed855a9f",
+          data: [
+            "겹벚꽃 앞에 피크닉 중이셨지만 사진 찍기엔 어려움이 없었다.",
+            "이미 다른 사람들이 줄 서서 사진을 찍고 있었다.",
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+// Data를 Id-Option별로 정제
+function processData(tmpData) {
+  let result = {};
+
+  tmpData.adDetection.forEach((node) => {
+    if (selectedOption.includes(node.option)) {
+      node.goodList.forEach((data) => {
+        if (!result[data.id]) {
+          result[data.id] = {
+            goodOption: [],
+            badOption: [],
+            flag: 0,
+          };
+        }
+        result[data.id].goodOption.push(node.option);
+      });
+
+      node.badList.forEach((data) => {
+        if (!result[data.id]) {
+          result[data.id] = {
+            goodOption: [],
+            badOption: [],
+            flag: 0,
+          };
+        }
+        result[data.id].badOption.push(node.option);
+      });
+    }
+  });
+
+  Object.keys(result).forEach((id) => {
+    let entry = result[id];
+    if (entry.goodOption.length > entry.badOption.length) {
+      entry.flag = 1;
+    } else if (entry.goodOption.length === entry.badOption.length) {
+      entry.flag = 0;
+    } else {
+      entry.flag = -1;
+    }
+  });
+
+  return result;
+}
+
+var finalResult = [];
+
 var checkInterval = setInterval(function () {
   var iframeDoc = iframe.contentWindow.document;
   var iframeElements = iframeDoc.getElementsByClassName("se-main-container");
-
-  var elementsArray = Array.from(iframeElements);
-
   if (iframeElements.length > 0) {
-    clearInterval(checkInterval); // 요소가 발견되면 setInterval 종료
-
-    // 모달 시작
-    const elements = iframeDoc.querySelectorAll(
-      ".se-main-container img, .se-main-container span, .se-main-container a"
-    );
+    clearInterval(checkInterval);
 
     function showModal(id, event) {
       let modal = iframeDoc.getElementById("hover-modal");
+      let message = "ID: " + id; // 기본 메시지
+
+      if (finalResult[id]) {
+        const data = finalResult[id];
+        let listContent = "";
+
+        if (data.flag === 1) {
+          message += " - 유용한 문장입니다.";
+          listContent = `<ul>${data.goodOption
+            .map((option) => `<li>${optionName[option - 1]}</li>`)
+            .join("")}</ul>`;
+        } else if (data.flag === 0) {
+          message += " - 중립적인 문장입니다.";
+          listContent = `<ul class='good-options'>Good Option : ${data.goodOption
+            .map((option) => `<li>${optionName[option - 1]}</li>`)
+            .join("")}</ul>
+                         <ul class='bad-options'>Bad Option :${data.badOption
+                           .map(
+                             (option) => `<li>${optionName[option - 1]}</li>`
+                           )
+                           .join("")}</ul>`;
+        } else if (data.flag === -1) {
+          message += " - 유해한 문장입니다.";
+          listContent = `<ul>${data.badOption
+            .map((option) => `<li>${optionName[option - 1]}</li>`)
+            .join("")}</ul>`;
+        }
+
+        message += listContent; // 리스트를 메시지에 추가
+      }
 
       if (!modal) {
         modal = iframeDoc.createElement("div");
@@ -26,7 +193,7 @@ var checkInterval = setInterval(function () {
         modal.style.zIndex = "1000";
         iframeDoc.body.appendChild(modal);
       }
-      modal.textContent = "ID: " + id;
+      modal.innerHTML = message; // textContent 대신 innerHTML 사용
       modal.style.display = "block"; // 먼저 보이게 하여 높이를 측정할 수 있도록 함
 
       const rect = event.target.getBoundingClientRect();
@@ -50,123 +217,39 @@ var checkInterval = setInterval(function () {
         modal.style.display = "none";
       }
     }
-
-    elements.forEach((element) => {
-      element.addEventListener("mouseover", function (event) {
-        event.stopPropagation();
-        var id = "";
-        if (event.target.tagName.toLowerCase() === "img") {
-          var dataLinkData =
-            event.target.parentNode.getAttribute("data-linkdata");
-          if (dataLinkData) {
-            var linkData = JSON.parse(dataLinkData);
-            id = linkData.id;
-          }
-        } else {
-          id = event.target.parentNode.id;
-        }
-        console.log(id);
-        showModal(id, event);
-      });
-
-      element.addEventListener("mouseout", function (event) {
-        hideModal();
-      });
-    });
     // 모달 end
 
-    // goodList - badList 출력
-    var divArray = Array.from(elementsArray[0].children);
-    var crawlResults = []; // 결과를 저장할 배열
+    // Coloring 시작
+    finalResult = processData(tmpData);
+    console.log(finalResult);
+    Object.keys(finalResult).forEach((id) => {
+      const data = finalResult[id];
+      const element = document
+        .getElementById("mainFrame")
+        .contentWindow.document.getElementById(id);
+      if (element) {
+        console.log(id + " " + finalResult[id]);
+        element.addEventListener("mouseover", function (event) {
+          event.stopPropagation();
+          showModal(id, event);
+        });
 
-    // 각 div 요소를 순회하면서 이미지와 텍스트 데이터 추출
-    divArray.forEach(function (div) {
-      var imgTags = div.getElementsByTagName("img");
-      var spanTags = div.getElementsByTagName("span");
+        element.addEventListener("mouseout", function (event) {
+          hideModal();
+        });
 
-      Array.from(imgTags).forEach(function (img) {
-        var dataLinkData = img.parentNode.getAttribute("data-linkdata");
-        if (dataLinkData) {
-          try {
-            var linkData = JSON.parse(dataLinkData);
-            var id = linkData.id;
-            if (id == null) return;
-            var src = linkData.src;
-            if (!src.includes("gif")) {
-              crawlResults.push({
-                type: "img",
-                data: src,
-                id: id,
-              });
-            }
-          } catch (e) {
-            console.error("JSON parsing error", e);
-          }
+        let html = element.innerHTML;
+
+        if (data.flag === 1) {
+          element.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // Green for good options
+        } else if (data.flag === 0) {
+          element.style.backgroundColor = "rgba(255, 235, 59, 0.3)"; // Yellow for neutral
+        } else {
+          element.style.backgroundColor = "rgba(241, 43, 67, 0.3)"; // Red for bad options
         }
-      });
 
-      Array.from(spanTags).forEach(function (span) {
-        var textContent = span.textContent || span.innerText;
-        var id = span.getAttribute("id");
-        if (id == null) return;
-        crawlResults.push({
-          type: "txt",
-          data: textContent,
-          id: id,
-        });
-      });
-    });
-    console.log(crawlResults);
-
-    // 크롬 익스텐션에 메시지 전송 및 응답 처리
-    chrome.runtime.sendMessage(
-      { action: "detail", crawlResults: crawlResults },
-      function (response) {
-        var listData = response.data.adDetection;
-        console.log(listData);
-
-        // goodList의 각 항목에 대해서 스타일 변경
-        listData.goodList.forEach(function (item) {
-          var element = document
-            .getElementById("mainFrame")
-            .contentWindow.document.getElementById(item.id);
-
-          if (element) {
-            var html = element.innerHTML;
-            item.data.forEach(function (data) {
-              if (data !== " ") {
-                html = html.replace(
-                  data,
-                  '<span style="background-color: rgba(66, 189, 101, 0.3)">' +
-                    data +
-                    "</span>"
-                );
-              }
-            });
-            element.innerHTML = html;
-          }
-        });
-
-        // badList의 각 항목에 대해서 스타일 변경
-        listData.badList.forEach(function (item) {
-          var element = document
-            .getElementById("mainFrame")
-            .contentWindow.document.getElementById(item.id);
-
-          if (element) {
-            var html = element.innerHTML;
-            item.data.forEach(function (data) {
-              html = html.replace(
-                data,
-                '<span style="background-color: rgba(241, 43, 67, 0.3)">' +
-                  data +
-                  "</span>"
-              );
-            });
-            element.innerHTML = html;
-          }
-        });
+        element.innerHTML = html;
       }
-    );
+    });
   }
-}, 100); // 100ms 마다 확인
+}, 100); // Check every 100ms
