7807b263cceb8df956a21f4b05b2ea65cd8fbfe3
이인석 <inkcloud365@gmail.com>
2024-05-08 16:33:12 +0900

    Feat: 비동기 처리에 따른 전역변수 제거
    Change-Id: Ifc0b8391018f9bd4037c8b8b03a686fb70d918e8

--- backend/services/option_service.py
+++ backend/services/option_service.py
@@ -1,5 +1,7 @@
 from kss import split_sentences
 import asyncio
+from typing import List, Any
+from pydantic import BaseModel
 
 from models.full_request import FullRequest
 from models.exception.custom_exception import CustomException
@@ -8,15 +10,16 @@ from services.naver_blog_scrap import NaverBlogScrapper
 from services.text_ad_detection import TextAdDetection
 
 
+class OptionParameters(BaseModel):
+    soup: Any
+    sentences: List[str]
+    keyword: str
+
+
 class OptionService:
     def __init__(self):
         self.cafeScrap = NaverCafeScrapper()
         self.blogScrap = NaverBlogScrapper()
-        self.list = None
-        self.soup = None
-        self.goodOption = None
-        self.badOption = None
-        self.keyword = None
         self._options = [
             self.calc_types_information,
             self.calc_bad_url,
@@ -26,11 +29,13 @@ class OptionService:
         ]
 
     async def option_service(self, data: FullRequest):
-        self.goodOption = data.goodOption
-        self.badOption = data.badOption
-        self.keyword = data.keyword
+        select = {
+            "good_option": data.goodOption,
+            "bad_option": data.badOption,
+            "keyword": data.keyword,
+        }
 
-        tasks = [self.url_score(url) for url in data.urlList]
+        tasks = [self.url_score(url, select) for url in data.urlList]
         results = await asyncio.gather(*tasks)
 
         return {
@@ -40,72 +45,82 @@ class OptionService:
             ]
         }
 
-    async def url_score(self, url: str):
+    async def url_score(self, url: str, select: dict):
         # url 스크랩
-        text = self.url_scrap(url)
+        text, soup = self.url_scrap(url)
         # 문장으로 나누기
-        await self.split_string(text)
+        sentences = split_sentences(text)
+
+        param = OptionParameters(
+            soup=soup, sentences=sentences, keyword=select["keyword"]
+        )
 
         # good option과 bad option 순서 맞춤
-        good_tasks = []
-        for option in self.goodOption:
+        tasks = []
+        for option in select["good_option"]:
             self._check_option_range(option)
-            good_tasks.append(self._options[option - 1]())
+            tasks.append(self._options[option - 1](param))
 
-        bad_tasks = []
-        for option in self.badOption:
+        for option in select["bad_option"]:
             self._check_option_range(option)
-            bad_tasks.append(self._options[option - 1]())
+            tasks.append(self._options[option - 1](param))
 
         # good option과 bad option을 한번에 await
-        result = await asyncio.gather(*(good_tasks + bad_tasks))
+        result = await asyncio.gather(*(tasks))
 
-        # 결과를 각 옵션 길이에 따라 슬라이싱해서 분배
         good_score, bad_score = 0, 0
-        if len(self.goodOption) > 0:
-            good_score += sum(result[: len(self.goodOption)]) / len(self.goodOption)
+        # 결과를 각 옵션 길이에 따라 슬라이싱해서 분배
+        good_option_length = len(select["good_option"])
+        if good_option_length > 0:
+            good_score += sum(result[:good_option_length]) / good_option_length
 
-        if len(self.badOption) > 0:
-            bad_score += sum(result[len(self.goodOption) :]) / len(self.goodOption)
+        bad_option_length = len(select["bad_option"])
+        if bad_option_length > 0:
+            bad_score += sum(result[good_option_length:]) / bad_option_length
 
         return good_score - bad_score
 
-    async def calc_types_information(self):
-        return await self.count_types_information() * 25
+    async def calc_types_information(self, param: OptionParameters):
+        return await self.count_types_information(param.soup) * 25
 
-    async def calc_bad_url(self):
-        return await self.has_bad_url() * 100
+    async def calc_bad_url(self, param: OptionParameters):
+        return await self.has_bad_url(param.soup) * 100
 
-    async def calc_not_sponsored_mark(self):
-        return await self.has_not_sponsored_mark() * 100
+    async def calc_not_sponsored_mark(self, param: OptionParameters):
+        return await self.has_not_sponsored_mark(param.soup) * 100
 
-    async def calc_contains_keyword(self):
-        return (await self.contains_keyword(self.keyword) / len(self.list)) * 100
+    async def calc_contains_keyword(self, param: OptionParameters):
+        return (
+            await self.contains_keyword(param.sentences, param.keyword)
+            / len(param.sentences)
+        ) * 100
 
-    async def calc_ad_detection(self):
-        return (await self.ad_detection() / len(self.list)) * 100
+    async def calc_ad_detection(self, param: OptionParameters):
+        return (await self.ad_detection(param.sentences) / len(param.sentences)) * 100
 
     def url_scrap(self, url: str):
-        text = ""
+        text = []
+        soup = None
+
         if "cafe" in url:  # 카페 게시글인 경우
-            self.soup = self.cafeScrap.scrape_naver_cafe_init(url)
-            text = self.cafeScrap.scrape_naver_cafe_text(self.soup)
+            soup = self.cafeScrap.scrape_naver_cafe_init(url)
+            text = self.cafeScrap.scrape_naver_cafe_text(soup)
 
         if "blog" in url:  # 블로그 게시글인 경우
-            self.soup = self.blogScrap.scrape_naver_blog_init(url)
-            text = self.blogScrap.scrape_naver_blog_text(self.soup)
+            soup = self.blogScrap.scrape_naver_blog_init(url)
+            text = self.blogScrap.scrape_naver_blog_text(soup)
 
-        return text
+        if len(text) < 1:
+            raise CustomException(status_code=400, message="텍스트를 찾을 수 없습니다")
 
-    async def split_string(self, paragraph: str):
-        self.list = split_sentences(paragraph)
+        return text, soup
 
-    def soup_get_main_container(self):
-        return self.soup.find("div", attrs={"class": "se-main-container"})
+    def soup_get_main_container(self, soup):
+        return soup.find("div", attrs={"class": "se-main-container"})
 
-    async def count_types_information(self):
+    async def count_types_information(self, soup):
         cnt = 0
-        soup = self.soup_get_main_container()
+        soup = self.soup_get_main_container(soup)
 
         # 사진 여부
         if soup.find("div", attrs={"class": "se-imageStrip"}) or soup.find(
@@ -124,7 +139,7 @@ class OptionService:
 
         return cnt
 
-    async def has_bad_url(self):
+    async def has_bad_url(self, soup):
         flag = 0
         blockUrlList = [
             "https://coupa.ng/",
@@ -132,7 +147,7 @@ class OptionService:
             "https://api3.myrealtrip.com/",
             "https://smartstore.naver.com",
         ]
-        soup = self.soup_get_main_container()
+        soup = self.soup_get_main_container(soup)
 
         div_oglink_tags = soup.find_all("div", attrs={"class": "se-oglink"})
 
@@ -145,30 +160,27 @@ class OptionService:
         # 1이면 블랙리스트 링크 존재
         return flag
 
-    async def has_not_sponsored_mark(self):
+    async def has_not_sponsored_mark(self, soup):
         flag = 0
-        if self.soup.find("div", attrs={"class": "not_sponsored_summary_wrap"}):
+        if soup.find("div", attrs={"class": "not_sponsored_summary_wrap"}):
             flag = 1
 
         # 1이면 내돈내산 마크 존재
         return flag
 
-    async def contains_keyword(self, keyword):
+    async def contains_keyword(self, sentences, keyword):
         cnt = 0
-        for i in self.list:
+        for i in sentences:
             if keyword in i:
                 cnt = cnt + 1
 
         return cnt
 
-    async def ad_detection(self):
-        cnt = 0
+    async def ad_detection(self, sentences):
         detector = TextAdDetection()
-        ad_result = detector.predict(self.list)
-        for i in range(len(self.list)):
-            if ad_result[i] == 1:
-                cnt = cnt + 1
-        return cnt
+        ad_result = detector.predict(sentences)
+
+        return ad_result.count(1)
 
     def _check_option_range(self, option):
         if 0 >= option or option > len(self._options):
