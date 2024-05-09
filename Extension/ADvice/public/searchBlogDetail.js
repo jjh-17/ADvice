23c77028393f97a03f504ce5af595eb83d42242b
Haeun Kim <gkdms1120s@gmail.com>
2024-05-09 11:31:16 +0900

    Feat : 선택된 Option 반영
    Change-Id: I767d7a67bd5e3b143380f208a794c1ec115f6a90

--- Extension/ADvice/public/searchBlogDetail.js
+++ Extension/ADvice/public/searchBlogDetail.js
@@ -166,198 +166,189 @@ chrome.storage.sync.get(["goodOption"], (result) => {
 
 function checkOption() {
   if (optionCnt == 2) {
-    console.log("bad");
-    selectedBadOption.forEach((element) => {
-      console.log(element);
-    });
-    console.log("good");
-    selectedGoodOption.forEach((element) => {
-      console.log(element);
-    });
-  }
-}
+    var checkInterval = setInterval(function () {
+      var iframeDoc = iframe.contentWindow.document;
+      var iframeElements =
+        iframeDoc.getElementsByClassName("se-main-container");
+      if (iframeElements.length > 0) {
+        clearInterval(checkInterval);
+        // 모달 시작
+        function showModal(id, event) {
+          let modal = iframeDoc.getElementById("hover-modal");
 
-var checkInterval = setInterval(function () {
-  var iframeDoc = iframe.contentWindow.document;
-  var iframeElements = iframeDoc.getElementsByClassName("se-main-container");
+          if (!modal) {
+            modal = iframeDoc.createElement("div");
+            modal.id = "hover-modal";
+            modal.style.position = "absolute";
+            modal.style.padding = "20px";
+            modal.style.background = "white";
+            modal.style.border = "1px solid black";
+            modal.style.zIndex = "1000";
+            modal.style.display = "none"; // 초기에는 보이지 않게 설정
+            iframeDoc.body.appendChild(modal);
+          }
 
-  if (iframeElements.length > 0) {
-    clearInterval(checkInterval);
+          const data = finalResult[id];
 
-    // 모달 시작
-    function showModal(id, event) {
-      let modal = iframeDoc.getElementById("hover-modal");
+          if (data) {
+            const statusMessage =
+              data.flag === 1
+                ? "선택하신 문장은 유용한 문장으로 판단됩니다 😀"
+                : data.flag === 0
+                ? "선택하신 문장은 중립적인 문장으로 판단됩니다 😐"
+                : "선택하신 문장은 유해한 문장으로 판단됩니다 😕";
 
-      if (!modal) {
-        modal = iframeDoc.createElement("div");
-        modal.id = "hover-modal";
-        modal.style.position = "absolute";
-        modal.style.padding = "20px";
-        modal.style.background = "white";
-        modal.style.border = "1px solid black";
-        modal.style.zIndex = "1000";
-        modal.style.display = "none"; // 초기에는 보이지 않게 설정
-        iframeDoc.body.appendChild(modal);
-      }
+            let goodOptionsList = "";
+            let badOptionsList = "";
 
-      const data = finalResult[id];
+            if (data.goodOption && (data.flag === 1 || data.flag === 0)) {
+              goodOptionsList =
+                `<div style="margin-top: 1.5625rem;">[긍정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;">` +
+                data.goodOption
+                  .map(
+                    (option) =>
+                      `<li style="margin-top: 0.3125rem;">• ${
+                        optionName[option - 1]
+                      }</li>`
+                  )
+                  .join("") +
+                "</ul></div>";
+            }
 
-      if (data) {
-        const statusMessage =
-          data.flag === 1
-            ? "선택하신 문장은 유용한 문장으로 판단됩니다 😀"
-            : data.flag === 0
-            ? "선택하신 문장은 중립적인 문장으로 판단됩니다 😐"
-            : "선택하신 문장은 유해한 문장으로 판단됩니다 😕";
+            if (data.badOption && (data.flag === -1 || data.flag === 0)) {
+              badOptionsList =
+                `<div style="margin-top: 1.5625rem;">[부정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;">` +
+                data.badOption
+                  .map(
+                    (option) =>
+                      `<li style="margin-top: 0.3125rem;">• ${
+                        optionName[option - 1]
+                      }</li>`
+                  )
+                  .join("") +
+                "</ul></div>";
+            }
 
-        let goodOptionsList = "";
-        let badOptionsList = "";
-
-        if (data.goodOption && (data.flag === 1 || data.flag === 0)) {
-          goodOptionsList =
-            `<div style="margin-top: 1.5625rem;">[긍정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;">` +
-            data.goodOption
-              .map(
-                (option) =>
-                  `<li style="margin-top: 0.3125rem;">• ${
-                    optionName[option - 1]
-                  }</li>`
-              )
-              .join("") +
-            "</ul></div>";
-        }
-
-        if (data.badOption && (data.flag === -1 || data.flag === 0)) {
-          badOptionsList =
-            `<div style="margin-top: 1.5625rem;">[부정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;">` +
-            data.badOption
-              .map(
-                (option) =>
-                  `<li style="margin-top: 0.3125rem;">• ${
-                    optionName[option - 1]
-                  }</li>`
-              )
-              .join("") +
-            "</ul></div>";
-        }
-
-        modal.innerHTML = `<div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+            modal.innerHTML = `<div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
           <div>
             <p style="text-align: center; font-weight: bold; margin-bottom: 10px;">${statusMessage}</p>
             ${goodOptionsList}
             ${badOptionsList}
           </div>
         </div>`;
-      }
+          }
 
-      modal.style.display = "block"; // 먼저 보이게 하여 높이를 측정할 수 있도록 함
+          modal.style.display = "block"; // 먼저 보이게 하여 높이를 측정할 수 있도록 함
 
-      const rect = event.target.getBoundingClientRect();
-      const scrollY =
-        iframe.contentWindow.pageYOffset ||
-        iframe.contentWindow.document.documentElement.scrollTop;
-      const scrollX =
-        iframe.contentWindow.pageXOffset ||
-        iframe.contentWindow.document.documentElement.scrollLeft;
+          const rect = event.target.getBoundingClientRect();
+          const scrollY =
+            iframe.contentWindow.pageYOffset ||
+            iframe.contentWindow.document.documentElement.scrollTop;
+          const scrollX =
+            iframe.contentWindow.pageXOffset ||
+            iframe.contentWindow.document.documentElement.scrollLeft;
 
-      // Adjust modal position to show above the element
-      modal.style.top = `${rect.top + scrollY - modal.offsetHeight - 10}px`; // 위치 조정
-      modal.style.left = `${rect.left + scrollX}px`;
-    }
-    function hideModal() {
-      const modal = iframeDoc.getElementById("hover-modal");
-      if (modal) {
-        modal.style.display = "none";
-      }
-    }
-    // 모달 끝
-
-    // 크롤링 시작
-    var elementsArray = Array.from(iframeElements);
-    var divArray = Array.from(elementsArray[0].children);
-    divArray.forEach(function (div) {
-      var imgTags = div.getElementsByTagName("img");
-      var spanTags = div.getElementsByTagName("span");
-
-      Array.from(imgTags).forEach(function (img) {
-        var dataLinkData = img.parentNode.getAttribute("data-linkdata");
-        if (dataLinkData) {
-          try {
-            var linkData = JSON.parse(dataLinkData);
-            var id = linkData.id;
-            if (id == null) return;
-            var src = linkData.src;
-            if (!src.includes("gif")) {
-              crawlResults.push({
-                type: "img",
-                data: src,
-                id: id,
-              });
-            }
-          } catch (e) {
-            console.error("JSON parsing error", e);
+          // Adjust modal position to show above the element
+          modal.style.top = `${rect.top + scrollY - modal.offsetHeight - 10}px`; // 위치 조정
+          modal.style.left = `${rect.left + scrollX}px`;
+        }
+        function hideModal() {
+          const modal = iframeDoc.getElementById("hover-modal");
+          if (modal) {
+            modal.style.display = "none";
           }
         }
-      });
+        // 모달 끝
 
-      Array.from(spanTags).forEach(function (span) {
-        var textContent = span.textContent || span.innerText;
-        var id = span.getAttribute("id");
-        if (id == null) return;
-        crawlResults.push({
-          type: "txt",
-          data: textContent,
-          id: id,
-        });
-      });
-    });
-    // 크롤링 end
+        // 크롤링 시작
+        var elementsArray = Array.from(iframeElements);
+        var divArray = Array.from(elementsArray[0].children);
+        divArray.forEach(function (div) {
+          var imgTags = div.getElementsByTagName("img");
+          var spanTags = div.getElementsByTagName("span");
 
-    // 인공지능 관련 데이터 받아오기
-    chrome.runtime.sendMessage(
-      { action: "detail", crawlResults: crawlResults },
-      function (response) {
-        var listData = response.data.adDetection;
-        var newData = {
-          option: 5,
-          goodList: listData.goodList,
-          badList: listData.badList,
-        };
-        tmpData.push(newData);
-        console.log(tmpData);
-
-        // Coloring 시작
-        finalResult = processData(tmpData);
-        console.log(finalResult);
-        Object.keys(finalResult).forEach((id) => {
-          const data = finalResult[id];
-          const element = document
-            .getElementById("mainFrame")
-            .contentWindow.document.getElementById(id);
-          if (element) {
-            element.addEventListener("mouseover", function (event) {
-              event.stopPropagation();
-              showModal(id, event);
-            });
-
-            element.addEventListener("mouseout", function (event) {
-              hideModal();
-            });
-
-            let html = element.innerHTML;
-
-            if (data.flag === 1) {
-              element.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // Green for good options
-            } else if (data.flag === 0) {
-              element.style.backgroundColor = "rgba(255, 235, 59, 0.3)"; // Yellow for neutral
-            } else {
-              element.style.backgroundColor = "rgba(241, 43, 67, 0.3)"; // Red for bad options
+          Array.from(imgTags).forEach(function (img) {
+            var dataLinkData = img.parentNode.getAttribute("data-linkdata");
+            if (dataLinkData) {
+              try {
+                var linkData = JSON.parse(dataLinkData);
+                var id = linkData.id;
+                if (id == null) return;
+                var src = linkData.src;
+                if (!src.includes("gif")) {
+                  crawlResults.push({
+                    type: "img",
+                    data: src,
+                    id: id,
+                  });
+                }
+              } catch (e) {
+                console.error("JSON parsing error", e);
+              }
             }
+          });
 
-            element.innerHTML = html;
-          }
+          Array.from(spanTags).forEach(function (span) {
+            var textContent = span.textContent || span.innerText;
+            var id = span.getAttribute("id");
+            if (id == null) return;
+            crawlResults.push({
+              type: "txt",
+              data: textContent,
+              id: id,
+            });
+          });
         });
+        // 크롤링 end
+        console.log(crawlResults);
+
+        // 인공지능 관련 데이터 받아오기
+        chrome.runtime.sendMessage(
+          { action: "detail", crawlResults: crawlResults },
+          function (response) {
+            var listData = response.data.adDetection;
+            var newData = {
+              option: 5,
+              goodList: listData.goodList,
+              badList: listData.badList,
+            };
+            tmpData.push(newData);
+            console.log(tmpData);
+
+            // Coloring 시작
+            finalResult = processData(tmpData);
+            console.log(finalResult);
+            Object.keys(finalResult).forEach((id) => {
+              const data = finalResult[id];
+              const element = document
+                .getElementById("mainFrame")
+                .contentWindow.document.getElementById(id);
+              if (element) {
+                element.addEventListener("mouseover", function (event) {
+                  event.stopPropagation();
+                  showModal(id, event);
+                });
+
+                element.addEventListener("mouseout", function (event) {
+                  hideModal();
+                });
+
+                let html = element.innerHTML;
+
+                if (data.flag === 1) {
+                  element.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // Green for good options
+                } else if (data.flag === 0) {
+                  element.style.backgroundColor = "rgba(255, 235, 59, 0.3)"; // Yellow for neutral
+                } else {
+                  element.style.backgroundColor = "rgba(241, 43, 67, 0.3)"; // Red for bad options
+                }
+
+                element.innerHTML = html;
+              }
+            });
+          }
+        );
       }
-    );
+    }, 100);
   }
-}, 100); // Check every 100ms
+}
