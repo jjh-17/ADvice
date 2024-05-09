4b80dd17da50471d28fd664cfcc73ebad23b7b22
Haeun Kim <gkdms1120s@gmail.com>
2024-05-09 16:02:30 +0900

    Feat: 3번 옵션 컬러링-모달 완성
    Change-Id: Ic1177d971f653611c7a7358ad8448ea9b96c263e

--- Extension/ADvice/public/searchBlogDetail.js
+++ Extension/ADvice/public/searchBlogDetail.js
@@ -14,86 +14,7 @@ var optionName = [
   "이모티콘 개수",
 ];
 
-var tmpData = [
-  {
-    option: 1,
-    goodList: [
-      {
-        id: "SE-600b5534-5478-40eb-93f6-66704272a14d",
-        data: ["겹벚꽃 보러,"],
-      },
-      {
-        id: "SE-e7e63fcf-f41c-439a-b75d-a67fdba06c27",
-        data: [
-          "3월 말부터 4월 초에 제주여행을 다녀왔다.",
-          "제주 여행 내내 비가 와서 제대로 벚꽃을 보지 못했는데, 서울에 오니 이미 벚꽃 엔딩이다.",
-        ],
-      },
-      {
-        id: "SE-8d9613c4-de90-4f58-ac2f-e944a918f834",
-        data: [
-          "요즘 꽃구경 다닐 때마다 인간 네비가 됐다.",
-          "우리는 조카가 있어서 주차를 하고 나와 분수대 쪽으로 나왔다.",
-          "분수를 앞에 두고 볼 때 왼편으로 걸어가면 바로 핑크빛 겹벚꽃을 만날 수 있다.",
-        ],
-      },
-    ],
-    badList: [
-      {
-        id: "SE-0dd3e82e-a5c4-4d0f-b623-eb87cc6fcfee",
-        data: [
-          "정말 아쉬웠다.",
-          "벚꽃을 이렇게 보내다니.",
-          "그래서 겹벚꽃 개화를 기다렸다.",
-          '그러다 막냇동생이 "언니 어린이대공원에 겹벚꽃 만개했대"',
-          "해서 알아보니 벌써 활짝 폈다.",
-        ],
-      },
-      {
-        id: "SE-6ff4b4f4-68cb-49cf-b59d-e265e69aaa18",
-        data: [
-          "급하게 날짜를 잡고 함께 다녀왔다.",
-          "겹벚꽃 소식을 알려준 막내는 약속이 있어 못 가고 첫째 동생이랑 조카와 함께 다녀왔다.",
-        ],
-      },
-    ],
-  },
-  {
-    option: 2,
-    goodList: [
-      {
-        id: "SE-8d9613c4-de90-4f58-ac2f-e944a918f834",
-        data: [
-          "요즘 꽃구경 다닐 때마다 인간 네비가 됐다.",
-          "우리는 조카가 있어서 주차를 하고 나와 분수대 쪽으로 나왔다.",
-          "분수를 앞에 두고 볼 때 왼편으로 걸어가면 바로 핑크빛 겹벚꽃을 만날 수 있다.",
-        ],
-      },
-      {
-        id: "SE-e569d924-0bb3-4da4-95fa-62e24b1c25f5",
-        data: [
-          "조금 놀랐던 건 작년 빼고 매해 방문했는데, 더 풍성했던 걸로 기억하는데 원래 찍던 나무는 꽃이 거의 없고 다른 장소에 한 그루 정도 낮게 있었다.",
-        ],
-      },
-    ],
-    badList: [
-      {
-        id: "SE-b785602a-1c29-406b-9794-2d375bda5854",
-        data: [
-          "큰 겹벚꽃 나무는 있는데 사진 찍을 만큼 낮으면서 풍성한 나무가 많이 사라진 느낌이었다.",
-          "내가 늦게 방문해서 겹벚꽃이 진 걸까 아니면 그냥 피지 못한 걸까 궁금했다.",
-        ],
-      },
-      {
-        id: "SE-d1b962d4-7b7b-4b30-b1d4-9b4eed855a9f",
-        data: [
-          "겹벚꽃 앞에 피크닉 중이셨지만 사진 찍기엔 어려움이 없었다.",
-          "이미 다른 사람들이 줄 서서 사진을 찍고 있었다.",
-        ],
-      },
-    ],
-  },
-];
+var tmpData = [];
 
 // Data를 Id-Option별로 정제
 function processData(tmpData) {
@@ -167,6 +88,80 @@ function optionFour(keyword) {
   tmpData.push(result);
 }
 
+function optionThree(iframeDoc) {
+  var elements = iframeDoc.querySelectorAll(
+    ".not_sponsored_component, .not_sponsored_summary"
+  );
+
+  var flag = selectedGoodOption.includes(3);
+  console.log(flag);
+
+  elements.forEach((element) => {
+    // 뒷 배경 색칠
+    const originalWidth = element.offsetWidth; // 원래 요소의 너비를 가져옵니다.
+    const newWidth = originalWidth + 30; // 원래 너비보다 20px 더 넓게 설정합니다.
+
+    // 배경색을 flag의 값에 따라 조정합니다.
+    const backgroundColor = flag
+      ? "rgba(66, 189, 101, 0.15)"
+      : "rgba(241, 43, 67, 0.15)"; // 초록색 또는 빨간색
+
+    // 래퍼 div를 생성하고 스타일을 설정합니다.
+    const wrapperHTML = `
+      <div class="custom-wrapper" style="width: ${newWidth}px; background-color: ${backgroundColor}; padding: 15px; box-sizing: border-box; margin: 0 auto;">
+      </div>
+    `;
+
+    // 요소 뒤에 래퍼를 삽입하고 요소를 그 안으로 이동시킵니다.
+    element.insertAdjacentHTML("afterend", wrapperHTML);
+    const wrapper = element.nextElementSibling;
+    wrapper.appendChild(element);
+
+    // 모달 띄우기
+    element.addEventListener("mouseover", function (event) {
+      let modal = iframeDoc.getElementById("hover-modal");
+      if (!modal) {
+        modal = iframeDoc.createElement("div");
+        modal.id = "hover-modal";
+        modal.style.cssText =
+          "position: absolute; padding: 20px; background: white; border: 1px solid black; z-index: 1000; display: none;";
+        iframeDoc.body.appendChild(modal);
+      }
+
+      let statusMessage = "";
+      let optionResult = "";
+      if (flag) {
+        statusMessage = "선택하신 부분은 유용한 부분으로 판단됩니다 😀";
+        optionResult = `<div style="margin-top: 1.5625rem;">[긍정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;"><li style="margin-top: 0.3125rem;">• 내돈내산 인증 포함</li></ul></div>`;
+      } else {
+        statusMessage = "선택하신 부분은 유해한 부분으로 판단됩니다 😕";
+        optionResult = `<div style="margin-top: 1.5625rem;">[부정적으로 평가된 요소]<ul style="list-style: none; padding-left: 0;"><li style="margin-top: 0.3125rem;">• 내돈내산 인증 포함</li></ul></div>`;
+      }
+
+      modal.innerHTML = `<div style="display: flex; flex-direction: column; align-items: center; justify-content: center;"><div><p style="text-align: center; font-weight: bold; margin-bottom: 10px;">${statusMessage}</p>${optionResult}</div></div>`;
+      modal.style.display = "block";
+
+      const rect = event.target.getBoundingClientRect();
+      const scrollY =
+        iframeDoc.defaultView.pageYOffset ||
+        iframeDoc.documentElement.scrollTop;
+      const scrollX =
+        iframeDoc.defaultView.pageXOffset ||
+        iframeDoc.documentElement.scrollLeft;
+
+      // Adjust modal position to show above the element
+      modal.style.top = `${rect.top + scrollY - modal.offsetHeight - 10}px`; // 위치 조정
+      modal.style.left = `${rect.left + scrollX}px`;
+    });
+    element.addEventListener("mouseout", function (event) {
+      const modal = iframeDoc.getElementById("hover-modal");
+      if (modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+}
+
 chrome.storage.sync.get(["badOption"], (result) => {
   if (result.badOption) {
     selectedBadOption = Object.values(result.badOption).map((obj) => obj.index); // "index" 값만 추출하여 배열에 추가
@@ -193,15 +188,6 @@ function checkOption() {
         iframeDoc.getElementsByClassName("se-main-container");
       if (iframeElements.length > 0) {
         clearInterval(checkInterval);
-
-        // 옵션3 관련 체크 코드
-        if (
-          iframeDoc.getElementsByClassName("not_sponsored_summary_wrap")
-            .length != 0
-        ) {
-          optionThreeflag = true;
-        }
-        console.log(optionThreeflag);
 
         // 모달 시작
         function showModal(id, event) {
@@ -332,52 +318,56 @@ function checkOption() {
         });
         // 크롤링 end
 
+        // 옵션 확인
+        optionFour("경주");
+        optionThree(iframeDoc);
+
         // 인공지능 관련 데이터 받아오기
-        // chrome.runtime.sendMessage(
-        //   { action: "detail", crawlResults: crawlResults },
-        //   function (response) {
-        //     var listData = response.data.adDetection;
-        //     var newData = {
-        //       option: 5,
-        //       goodList: listData.goodList,
-        //       badList: listData.badList,
-        //     };
-        //     tmpData.push(newData);
-        //     console.log(tmpData);
+        chrome.runtime.sendMessage(
+          { action: "detail", crawlResults: crawlResults },
+          function (response) {
+            var listData = response.data.adDetection;
+            var newData = {
+              option: 5,
+              goodList: listData.goodList,
+              badList: listData.badList,
+            };
+            tmpData.push(newData);
+            console.log(tmpData);
 
-        //     // Coloring 시작
-        //     finalResult = processData(tmpData);
-        //     console.log(finalResult);
-        //     Object.keys(finalResult).forEach((id) => {
-        //       const data = finalResult[id];
-        //       const element = document
-        //         .getElementById("mainFrame")
-        //         .contentWindow.document.getElementById(id);
-        //       if (element) {
-        //         element.addEventListener("mouseover", function (event) {
-        //           event.stopPropagation();
-        //           showModal(id, event);
-        //         });
+            // Coloring 시작
+            finalResult = processData(tmpData);
+            console.log(finalResult);
+            Object.keys(finalResult).forEach((id) => {
+              const data = finalResult[id];
+              const element = document
+                .getElementById("mainFrame")
+                .contentWindow.document.getElementById(id);
+              if (element) {
+                element.addEventListener("mouseover", function (event) {
+                  event.stopPropagation();
+                  showModal(id, event);
+                });
 
-        //         element.addEventListener("mouseout", function (event) {
-        //           hideModal();
-        //         });
+                element.addEventListener("mouseout", function (event) {
+                  hideModal();
+                });
 
-        //         let html = element.innerHTML;
+                let html = element.innerHTML;
 
-        //         if (data.flag === 1) {
-        //           element.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // Green for good options
-        //         } else if (data.flag === 0) {
-        //           element.style.backgroundColor = "rgba(255, 235, 59, 0.3)"; // Yellow for neutral
-        //         } else {
-        //           element.style.backgroundColor = "rgba(241, 43, 67, 0.3)"; // Red for bad options
-        //         }
+                if (data.flag === 1) {
+                  element.style.backgroundColor = "rgba(66, 189, 101, 0.3)"; // Green for good options
+                } else if (data.flag === 0) {
+                  element.style.backgroundColor = "rgba(255, 235, 59, 0.3)"; // Yellow for neutral
+                } else {
+                  element.style.backgroundColor = "rgba(241, 43, 67, 0.3)"; // Red for bad options
+                }
 
-        //         element.innerHTML = html;
-        //       }
-        //     });
-        //   }
-        // );
+                element.innerHTML = html;
+              }
+            });
+          }
+        );
       }
     }, 100);
   }
