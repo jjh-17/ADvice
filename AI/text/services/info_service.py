583b66c34180348ad57a4bb39cc0e31ab4dba213
오주현 <ojh1105@naver.com>
2024-05-10 17:05:51 +0900

    fix: infoDetector 내 함수명 매칭 문제 해결
    Change-Id: I39007f77ef08d3536b5695ce56321fd6f5b47018

--- AI/text/services/info_service.py
+++ AI/text/services/info_service.py
@@ -3,7 +3,7 @@ from internals.text_analyzer import infoDetector
 
 class InfoService:
     def detail_info_detection(self, texts: list):
-        return infoDetector.detect_texts(texts)
+        return infoDetector.detect(texts)
 
 
 infoService = InfoService()
