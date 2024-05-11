a32c2ceb4ed5d6afd515affa216d7a92b85ce32e
지준호 <jjho311@naver.com>
2024-05-11 16:21:27 +0900

    Fix: pydantic_core validation 오류

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,6 +6,13 @@ class Settings(BaseSettings):
     emotion_host: str
     text_ad_host: str
     image_ad_host: str
+    fasttext_model_path: str
+    target_language_code: str
+    ad_detection_model_path: str
+    info_detection_model_path: str
+    yolo_model_path: str
+    pretrained_tokenizer: str
+    user_agent: str
 
     class Config:
         env_file = ".env"
