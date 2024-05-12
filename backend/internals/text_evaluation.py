4107081885d9160c5f61a6a7d0fdd5079b13b2de
0o0mlb <gw07167@naver.com>
2024-05-12 20:34:29 +0900

    remove: 불필요한 파일 제거

--- backend/internals/text_evaluation.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-from config.config import settings
-
-class TextEvaluation:
-    async def ad_evaluate_short(self, data):
-        res = requests.post(url=settings.emotion_host + "/emotion", data=json.dumps(data))
-        return res.json()
