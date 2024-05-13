84b918f26e61fe4276b8188dbd884a04fa18be48
이인석 <inkcloud365@gmail.com>
2024-05-13 15:26:06 +0900

    Revert "Feat: Profile 코드 추가"
    This reverts commit 0b9e9bf563390d040b3dc6cf7dbacd27cdc7659c.

--- AI/text/main.py
+++ AI/text/main.py
@@ -4,11 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.text_ad_evaluation import text
 from routers.text_info_evaluation import info
 
-#TODO: 프로파일링 완료 후 삭제 요망
-import cProfile
-import io
-import pstats
-
 app = FastAPI()
 
 app.add_middleware(
@@ -18,21 +13,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-#TODO: 프로파일링 완료 후 삭제 요망
-@app.middleware("http")
-async def cprofile_middleware(request, call_next):
-    profiler = cProfile.Profile()
-    profiler.enable()  # 프로파일링 시작
-    response = await call_next(request)
-    profiler.disable()  # 프로파일링 정지
-    s = io.StringIO()
-    sortby = "cumulative"  # 누적 시간으로 정렬
-    ps = pstats.Stats(profiler, stream=s).strip_dirs().sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())  # 콘솔에 출력
-    return response
-
 
 app.include_router(text)
 app.include_router(info)
