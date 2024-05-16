8b1bef6c8297bc8183feb6c2699e683933ada61f
오주현 <ojh1105@naver.com>
2024-05-16 12:27:28 +0900

    feat: ad, info detection 반환 값 설정
    Change-Id: I15f7fda350f155fb417cb14f350b1ee93781ac79

--- AI/text/routers/text_ad_evaluation.py
+++ AI/text/routers/text_ad_evaluation.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from typing import List
 from starlette.responses import JSONResponse
 
+from models.detail_request import DetailRequest
 from models.exception.custom_exception import CustomException
 from services.ad_service import adService
 
@@ -12,7 +13,9 @@ text = APIRouter()
 def text_ad_evaluate(data: List[str]):
     if not data:
         raise CustomException(422, "No texts provided")
-    return JSONResponse(status_code=200, content=adService.ad_evaluation(data))
+
+    preds, scores = adService.ad_evaluation(data)
+    return JSONResponse(status_code=200, content=DetailRequest(prediction=preds, score=scores).dict())
 
 
 @text.post("/ad-evaluate/short")
--- AI/text/routers/text_info_evaluation.py
+++ AI/text/routers/text_info_evaluation.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from typing import List
 from starlette.responses import JSONResponse
 
+from models.detail_request import DetailRequest
 from models.exception.custom_exception import CustomException
 from services.info_service import infoService
 
@@ -12,4 +13,6 @@ info = APIRouter()
 def text_ad_evaluate(data: List[str]):
     if not data:
         raise CustomException(422, "No texts provided")
-    return JSONResponse(status_code=200, content=infoService.detail_info_detection(data))
+
+    preds, scores = infoService.detail_info_detection(data)
+    return JSONResponse(status_code=200, content=DetailRequest(prediction=preds, score=scores).dict())
